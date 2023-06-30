dd6d7d0d443c837c96e5a5cbc38a46beb4271412
tom0267 <ditchfit@lancaster.ac.uk>
2023-06-30 17:53:31 +0100

    added quit key

--- Tracking.py
+++ Tracking.py
@@ -3,15 +3,18 @@ import cv2
 cap = cv2.VideoCapture("tomAndSuzi.mp4")
 
 #object detection from stable camera
-object_detector = cv2.createBackgroundSubtractorMOG2()
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=20)
 
 while True:
     ret, frame = cap.read()
 
     mask = object_detector.apply(frame)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    #extract region of interest
+    #roi = frame[340: 600,500: 700]
 
-    
-    
+
+
     #object detection
     #cv2.imshow("Mask", mask)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -21,10 +24,12 @@ while True:
         if area > 250:
             cv2.drawContours(frame, [cnt], -1, (0,255,0), 2)
 
-    key = cv2.waitKey(30)
-    if key == 27:
-        cap.release()
-        cv2.destroyAllWindows()
+    #key = cv2.waitKey(30)
+    if cv2.waitKey(30) & 0xFF ==ord('q'):
+        break
 
     cv2.imshow("Frame", frame)
+
+cap.release()
+cv2.destroyAllWindows()
 
