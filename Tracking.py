68a484c8b87f67a5776e81a310afb2b9cac29cd4
tom0267 <ditchfit@lancaster.ac.uk>
2023-07-02 17:40:56 +0100

    added face detection

--- Tracking.py
+++ Tracking.py
@@ -15,42 +15,46 @@ cap.set(10,100)     #set brightness
 #object detection from stable camera
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=20)
 
-faceCascade= cv2.CascadeClassifier("haarcascade_eye.xml")
+eyeCascade = cv2.CascadeClassifier("Resources/haarcascade_eye.xml")
+faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
 
 while True:
-    ret, frame = cap.read()
+    ret, Webcam = cap.read()
 
-    mask = object_detector.apply(frame)
-    grey = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    mask = object_detector.apply(Webcam)
+    grey = cv2.cvtColor(Webcam,cv2.COLOR_BGR2GRAY)
     cv2.imshow("Grey", grey)
     #_, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     #extract region of interest
-    #roi = frame[340: 600,500: 700]
+    #roi = Webcam[340: 600,500: 700]
 
 
 
     #object detection
-    #cv2.imshow("Mask", mask)
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        #calculate area and remove small objects
-        area = cv2.contourArea(cnt)
-        if area > 100:
-            cv2.drawContours(frame, [cnt], -1, (0,255,0), 2)
+    #contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #for cnt in contours:
+    #    #calculate area and remove small objects
+    #    area = cv2.contourArea(cnt)
+    #    if area > 100:
+    #        cv2.drawContours(Webcam, [cnt], -1, (0,255,0), 2)
     
-    #greyFrame = cv2.cvtColor(frame,cv2.COLOR_BAYER_BG2GRAY)
+    #detect eyes and faces using the cascade
+    eyes = eyeCascade.detectMultiScale(grey,1.1,4)
     faces = faceCascade.detectMultiScale(grey,1.1,4)
     
+    #draw rectangles around eyes and faces
+    for(x,y,w,h) in eyes:
+        cv2.rectangle(Webcam, (x,y), (x+w,y+h),(255,0,0),3)
     
     for(x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w,y+h),(255,0,0),3)
+        cv2.rectangle(Webcam, (x,y), (x+w,y+h),(255,0,0),3)
 
 
     #key = cv2.waitKey(30)
     if cv2.waitKey(30) & 0xFF ==ord('q'):
         break
 
-    cv2.imshow("Frame", frame)
+    cv2.imshow("Webcam", Webcam)
 
 cap.release()
 cv2.destroyAllWindows()
