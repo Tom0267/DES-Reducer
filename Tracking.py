7b5694cfb99539a9f69b379cc17bb9722be75db7
tom0267 <ditchfit@lancaster.ac.uk>
2023-07-02 14:02:47 +0100

    eye detection

--- Tracking.py
+++ Tracking.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 
 #video
 #cap = cv2.VideoCapture("tomAndSuzi.mp4")
@@ -13,11 +15,15 @@ cap.set(10,100)     #set brightness
 #object detection from stable camera
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=20)
 
+faceCascade= cv2.CascadeClassifier("haarcascade_eye.xml")
+
 while True:
     ret, frame = cap.read()
 
     mask = object_detector.apply(frame)
-    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    grey = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Grey", grey)
+    #_, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     #extract region of interest
     #roi = frame[340: 600,500: 700]
 
@@ -31,6 +37,14 @@ while True:
         area = cv2.contourArea(cnt)
         if area > 100:
             cv2.drawContours(frame, [cnt], -1, (0,255,0), 2)
+    
+    #greyFrame = cv2.cvtColor(frame,cv2.COLOR_BAYER_BG2GRAY)
+    faces = faceCascade.detectMultiScale(grey,1.1,4)
+    
+    
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h),(255,0,0),3)
+
 
     #key = cv2.waitKey(30)
     if cv2.waitKey(30) & 0xFF ==ord('q'):
