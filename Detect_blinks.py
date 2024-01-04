716aeec955f695b1b07ed671361fc19d6f776307
tom0267 <ditchfit@lancaster.ac.uk>
2024-01-04 23:16:04 +0000

    added a screen brightness controller

--- Detect_blinks.py
+++ Detect_blinks.py
@@ -1,3 +1,4 @@
+from ScreenBrightness import BrightnessControl
 from scipy.spatial import distance as dist
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
@@ -5,22 +6,24 @@ from win10toast import ToastNotifier
 from imutils import face_utils
 from time import time
 import numpy as np
+import threading
 import imutils
 import time
 import dlib
 import cv2
 
-# compute the euclidean distances between the two sets of vertical eye landmarks (x, y) coordinates
+#calculate the euclidean distances between the two sets of vertical eye landmarks (x, y) coordinates
 def eye_aspect_ratio(eye):
 	A = dist.euclidean(eye[1], eye[5])
 	B = dist.euclidean(eye[2], eye[4])
-	C = dist.euclidean(eye[0], eye[3])  					#compute the euclidean distance between the horizontal eye landmark (x, y)-coordinates
-	ear = (A + B) / (2.0 * C)  		 						#compute the eye aspect ratio
+	C = dist.euclidean(eye[0], eye[3])  					#calculate the euclidean distance between the horizontal eye landmark (x, y)-coordinates
+	ear = (A + B) / (2.0 * C)  		 						#calculate the eye aspect ratio
 	return ear
 
+brightnessControl = BrightnessControl()		#initialize the brightness control class
 notifier = ToastNotifier()					#initialize the notifier
-blinkThresh = 0.26						#threshold for eye aspect ratio to count as a blink
-squintThresh = 0.3						#threshold for eye aspect ratio to count as a squint
+blinkThresh = 0.25						#threshold for eye aspect ratio to count as a blink
+squintThresh = 0.29						#threshold for eye aspect ratio to count as a squint
 blinkConsecFrames = 2					#number of consecutive frames the eye must be below the threshold for to count as a blink
 squintConsecFrames = 10				#number of consecutive frames the eye must be below the threshold for to count as a squinting
 blinkCounter = 0									#frame blink Counter
@@ -37,6 +40,8 @@ blink1 = np.datetime64('now')
 blink2 = np.datetime64('now')
 while True:
 	frame = vs.read()
+	brightness = threading.Thread(brightnessControl.update(frame))					#start the brightness control thread
+	brightness.start()									#start the brightness control thread
 	#frame = imutils.resize(frame, width=450)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	
@@ -45,42 +50,45 @@ while True:
 		shape = predictor(gray, face)					#determine the facial landmarks for the face region, then convert the facial landmark (x, y)-coordinates to a NumPy array
 		shape = face_utils.shape_to_np(shape)
 		
-		leftEye = shape[lStart:lEnd]					#extract the left and right eye coordinates, then use the coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]					#extract the left and right eye coordinates, then use the coordinates to calculate the eye aspect ratio for both eyes
 		rightEye = shape[rStart:rEnd]
 		leftEAR = eye_aspect_ratio(leftEye)				#left eye aspect ratio
 		rightEAR = eye_aspect_ratio(rightEye)			#right eye aspect ratio
 		
 		ear = (leftEAR + rightEAR) / 2.0				#average the eye aspect ratio together for both eyes
 		
-		leftEyeHull = cv2.convexHull(leftEye)			#compute the convex hull for the left eye
-		rightEyeHull = cv2.convexHull(rightEye)			#compute the convex hull for the right eye
+		leftEyeHull = cv2.convexHull(leftEye)			#calculate the convex hull for the left eye
+		rightEyeHull = cv2.convexHull(rightEye)			#calculate the convex hull for the right eye
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)			#draw the hull around the left eye
 		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)			#draw the hull around the right eye
 
-		if ear <= blinkThresh:								#check is eye aspect ratio is below the blink threshold
+		if ear <= blinkThresh:										#check is eye aspect ratio is below the blink threshold
 			blinkCounter += 1										#increment the blink Counter
 			if blinkCounter >= blinkConsecFrames and not blinkCounter > 3:			#if the eyes were closed for a sufficient number of frames increment the total number of blinks
 				total += 1
-				blink2 = np.datetime64('now')
-				if blink2 - blink1 >5:
-					notifier.show_toast("BLINK BITCH!!", "For healthy eyes, you should be blinking every 5 seconds.", duration=20, threaded=True)
+				blink2 = np.datetime64('now')					#record the time of the blink
+				if blink2 - blink1 >  8:						#check if the time between blinks is greater than 8 seconds
+					notifier.show_toast("Don't Forget To Blink", "For healthy eyes, you should be blinking every 5 seconds.", duration=5, threaded=True)
+				elif blink2 - blink1 < 2:						#check if the time between blinks is less than 2
+					notifier.show_toast("Rapid Blinking", "Take a break from the screen to ensure your eyes stay healythy.", duration=5, threaded=True)
 		else :
 			blinkCounter = 0								#reset the eye frame blink Counter
 			blink1 = blink2
 
-		if ear <= squintThresh:								#check is eye aspect ratio is below the squint threshold
+		if ear <= squintThresh:										#check is eye aspect ratio is below the squint threshold
 			squintCounter += 1										#increment the squint Counter
-			if squintCounter >= squintConsecFrames:				#if the eyes were closed for a sufficient number of frames increment the total number of squints
-				notifier.show_toast("Squinting Detected", "Ensure you have proper lighting and are not too close to the screen.", duration=20, threaded=True)		#display tray notification
+			if squintCounter >= squintConsecFrames:					#if the eyes were closed for a sufficient number of frames increment the total number of squints
+				notifier.show_toast("Squinting Detected", "Ensure you have proper lighting and are not too close to the screen.", duration=5, threaded=True)		#display tray notification
 		else :
 			squintCounter = 0								#reset the eye frame squint Counter
 
 		cv2.putText(frame, "Blinks: {}".format(total), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)				#draw the total number of blinks on the frame
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)				#draw the computed eye aspect ratio on the frame
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)				#draw the calculated eye aspect ratio on the frame
 		cv2.imshow("Eye Care", frame)						#show the frame
+	brightness.join()									#join the brightness control thread
 
-		if cv2.waitKey(30) & 0xFF ==ord('q'):				#press q to quit
-			break
+	if cv2.waitKey(30) & 0xFF ==ord('q'):				#press q to quit
+		break
 
 cv2.destroyAllWindows()								#close all windows
 vs.stop()											#stop the video stream
---
+++ ScreenBrightness.py
@@ -0,0 +1,27 @@
+import cv2
+import screen_brightness_control as sbc
+
+class BrightnessControl:
+
+    def cameraExposure(self):
+        camera = cv2.VideoCapture(self.cameraIndex, cv2.CAP_DSHOW)              #initialize the camera
+        camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)                        # 0.25 turns OFF auto exposure         
+        camera.set(cv2.CAP_PROP_EXPOSURE, self.exposureValue)                   #sets the exposure value of the camera    
+
+    def calculateBrightness(self, image):
+        average_brightness = (cv2.mean(image)[0] / 255.0) *20               #calculate the average brightness of the image
+        return average_brightness
+
+    def setBrightness(self, brightness):
+        brightness = brightness + self.brightness_offset                          #adjust brightness
+        sbc.set_brightness(brightness)                                      #set brightness
+
+    def update(self, frame):
+        brightness = self.calculateBrightness(frame) * 100
+        self.setBrightness(brightness)
+
+    def __init__(self):
+        self.brightness_offset = 20                     #adjust to offset the brightness up/down. 
+        self.cameraIndex = 0                            #select camera
+        self.exposureValue = 10                          #use in with brightness_offset. A lower number means lower brightness.
+        self.cameraExposure()
