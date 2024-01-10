3a4b9e7ef63a5c6054323bb87d8dae6593919b0f
tom0267 <ditchfit@lancaster.ac.uk>
2024-01-10 23:58:03 +0000

    minor refactoring

--- Detect_blinks.py
+++ Detect_blinks.py
@@ -13,8 +13,8 @@ import time
 import dlib
 import cv2
 
-brightnessControl = BrightnessControl()		#initialize the brightness control class
-notifier = ToastNotifier()			#initialize the notifier
+brightnessControl = BrightnessControl()					#initialize the brightness control class
+notifier = ToastNotifier()								#initialize the notifier
 eyeMovement = EyeMovement(notifier)						#initialize the eye movement class
 breakCheck = breakTime(notifier)						#initialize the break check class
 
@@ -30,34 +30,33 @@ while True:
 	breakTime = threading.Thread(breakCheck.checkBreak())							#start the break check thread
 	brightness.start()											#start the brightness control thread
 	breakTime.start()											#start the break check thread
-	#frame = imutils.resize(frame, width=450)
+	frame = imutils.resize(frame, width=450)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	
 	faces = detector(gray, 0)									#detect faces in the grayscale frame
 	if not faces:											#check if a face was detected
 		notifier.show_toast("No Face Detected", "Ensure your face is in the frame.", duration=5, threaded=True)		#display tray notification
-		pass
+	else:
+		for face in faces:
+			shape = predictor(gray, face)							#determine the facial landmarks for the face region, then convert the facial landmark (x, y)-coordinates to a NumPy array
+			shape = face_utils.shape_to_np(shape)
+			
+			leftEye = shape[lStart:lEnd]							#extract the left and right eye coordinates, then use the coordinates to calculate the eye aspect ratio for both eyes
+			rightEye = shape[rStart:rEnd]
+			eyeMovement.checkMovement(leftEye,rightEye)				#check if the eyes are blinking or squinting
+			leftEyeHull, rightEyeHull = eyeMovement.getHull()
+			cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)			#draw the hull around the left eye
+			cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)			#draw the hull around the right eye
+			total = eyeMovement.getTotal()
+			ear = eyeMovement.getEAR()
+			cv2.putText(frame, "Blinks: {}".format(total), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)				#draw the total number of blinks on the frame
+			cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)				#draw the calculated eye aspect ratio on the frame
+		cv2.imshow("Eye Care", frame)						#show the frame
+		brightness.join()									#join the brightness control thread
+		breakTime.join()									#join the break check thread
 
-	for face in faces:
-		shape = predictor(gray, face)							#determine the facial landmarks for the face region, then convert the facial landmark (x, y)-coordinates to a NumPy array
-		shape = face_utils.shape_to_np(shape)
-		
-		leftEye = shape[lStart:lEnd]							#extract the left and right eye coordinates, then use the coordinates to calculate the eye aspect ratio for both eyes
-		rightEye = shape[rStart:rEnd]
-		eyeMovement.checkMovement(leftEye,rightEye)				#check if the eyes are blinking or squinting
-		leftEyeHull, rightEyeHull = eyeMovement.getHull()
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)			#draw the hull around the left eye
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)			#draw the hull around the right eye
-		total = eyeMovement.getTotal()
-		ear = eyeMovement.getEAR()
-		cv2.putText(frame, "Blinks: {}".format(total), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)				#draw the total number of blinks on the frame
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)				#draw the calculated eye aspect ratio on the frame
-	cv2.imshow("Eye Care", frame)						#show the frame
-	brightness.join()									#join the brightness control thread
-	breakTime.join()									#join the break check thread
-
-	if cv2.waitKey(30) & 0xFF ==ord('q'):				#press q to quit
-		break
+		if cv2.waitKey(30) & 0xFF ==ord('q'):				#press q to quit
+			break
 
 cv2.destroyAllWindows()								#close all windows
 vs.stop()		
--- FaceTrack.py
+++ FaceTrack.py
@@ -15,13 +15,9 @@ dlib.train_shape_predictor(training_xml_path, "predictor.dat", options)         
 
 print("\nTraining accuracy: {}".format(dlib.test_shape_predictor(training_xml_path, "predictor.dat")))                      #prints out training accuracy
 
-
-
 predictor = dlib.shape_predictor("predictor.dat")
 detector = dlib.get_frontal_face_detector()
 
-# Now let's run the detector and shape_predictor over the images in the faces
-# folder and display the results.
 print("Showing detections and predictions on the images in the faces folder...")
 win = dlib.image_window()
 for f in glob.glob(os.path.join(faces_folder, "*.jpg")):
@@ -37,14 +33,12 @@ for f in glob.glob(os.path.join(faces_folder, "*.jpg")):
     dets = detector(img, 1)
     print("Number of faces detected: {}".format(len(dets)))
     for k, d in enumerate(dets):
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-            k, d.left(), d.top(), d.right(), d.bottom()))
-        # Get the landmarks/parts for the face in box d.
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(k, d.left(), d.top(), d.right(), d.bottom()))
+        #get the landmarks/parts for the face in box d.
         shape = predictor(img, d)
-        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
-                                                  shape.part(1)))
-        # Draw the face landmarks on the screen.
-        win.add_overlay(shape)
+        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),shape.part(1)))
+        
+        win.add_overlay(shape)      #draw the face landmarks on the screen.
 
     win.add_overlay(dets)
     dlib.hit_enter_to_continue()
